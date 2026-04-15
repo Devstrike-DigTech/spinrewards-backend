168603fd5e0a764c60acc81104e6d28adc80c658
Richard Uzor <equality560@gmail.com>
2026-04-15 16:15:07 +0100

    Fix invalid default_auto_field in UsersConfig
    UUIDField is not an AutoField subclass and cannot be used as
    default_auto_field. UUID PKs are already explicitly defined on
    each model so this setting was redundant and caused boot failure.

--- apps/users/apps.py
+++ apps/users/apps.py
@@ -3,7 +3,6 @@ from django.apps import AppConfig
 
 class UsersConfig(AppConfig):
     name = 'apps.users'
-    default_auto_field = 'django.db.models.UUIDField'
 
     def ready(self):
         pass  # import signals here if needed
